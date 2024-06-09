652278fe06ce4e561753b111bdc0efda2a5f4578
Mahiyat <mahiyat.tanzim@gmail.com>
2024-06-09 19:25:49 +0600

    Categorize About section
    Add second project

--- mahiyat-portfolio/src/components/About.jsx
+++ mahiyat-portfolio/src/components/About.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Container, Typography } from '@mui/material';
+import {
+  Container,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+} from '@mui/material';
 
 const About = () => {
   return (
@@ -10,13 +17,73 @@ const About = () => {
       <Typography variant="h4" gutterBottom>
         About Me
       </Typography>
-      <Typography>
-        I am Mahiyat Tanzim. I am an undergraduate currently in 3rd year 1st
-        semester at the department of Computer Science and Engineering in
-        Jahangirnagar University. I have skills in problem solving, programming
-        language, Microsoft office, internet surfing, supervision, presentation,
-        leadership, planning, self-learning, and technical communication. I
-        worked part-time as a warranty wing leader in Pro 2 Interactive Ltd.
+      <Typography align="justify" paragraph>
+        My name is Mahiyat Tanzim, and I am an undergraduate student in my
+        fourth year, first semester, at the Department of Computer Science and
+        Engineering at Jahangirnagar University. I have a keen interest in
+        software engineering and teaching, and I am passionate about
+        continuously learning new things.
+      </Typography>
+      <Divider sx={{ my: 2 }} />
+      <Typography variant="h5" gutterBottom>
+        Skills
+      </Typography>
+      <List>
+        <ListItem>
+          <ListItemText primary="Problem Solving" />
+        </ListItem>
+        <ListItem>
+          <ListItemText
+            primary="Programming Languages"
+            secondary="Java, Python, C++"
+          />
+        </ListItem>
+        <ListItem>
+          <ListItemText
+            primary="Frontend Technologies"
+            secondary="HTML, CSS, Javascript, React"
+          />
+        </ListItem>
+        <ListItem>
+          <ListItemText
+            primary="Backend Technologies"
+            secondary="Django, Nodejs"
+          />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Prompt Engineering" />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Microsoft Office" />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Internet Surfing" />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Supervision" />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Presentation" />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Leadership" />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Planning" />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Self-learning" />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Technical Communication" />
+        </ListItem>
+      </List>
+      <Divider sx={{ my: 2 }} />
+      <Typography variant="h5" gutterBottom>
+        Experience
+      </Typography>
+      <Typography paragraph>
+        I worked part-time as a warranty wing leader in Pro 2 Interactive Ltd.
         from August 2020 to October 2020.
       </Typography>
     </Container>
--- mahiyat-portfolio/src/components/Navigation.jsx
+++ mahiyat-portfolio/src/components/Navigation.jsx
@@ -3,13 +3,19 @@ import { AppBar, Toolbar, Button } from '@mui/material';
 
 const Navigation = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="fixed">
       <Toolbar>
         <Button color="inherit" href="#about">
           About
         </Button>
         <Button color="inherit" href="#project">
           Projects
+        </Button>
+        <Button color="inherit" href="#contest">
+          Contests
+        </Button>
+        <Button color="inherit" href="#research">
+          Researches
         </Button>
       </Toolbar>
     </AppBar>
